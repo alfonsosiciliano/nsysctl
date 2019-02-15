841cefbf1938fcf5fa52a4ed58546a45ce225ef5
Alfonso Siciliano <alfix86@gmail.com>
2019-02-15 21:37:55 +0100

    initial vm.phys_free xo-ish

--- special_value.c
+++ special_value.c
@@ -27,19 +27,52 @@
  * 'Special values' are values splitted for xo-output
  */
 
+#include <stdbool.h>
 #include <string.h>
 #include <libxo/xo.h>
 
 #include "special_value.h"
 
-static bool get_next_int(int);
 static int vm_phys_free(void* value, size_t value_size);
+
+static bool find_int(char *start, char *end, int *intvalue)
+{
+    int j=0,i=0;
+    char *s,*e, *tmp;
+    long long llvalue;
+
+    s=start;
+
+    while (true) {
+	if(s[i] == '\n' || s[i] == '\0' || s[i] == EOF)
+	    return false;
+	if(s[i] >= '0' && s[i] <= '9')
+	    break;
+	i++;
+    }
+
+    e = &s[i];
+
+    while (true) {
+	if(e[j] == '\n' || e[j] == '\0' || e[j] == EOF || e[j] < '0' || e[j] > '9')
+	    break;
+	j++;
+    }
+
+    tmp = strndup(&s[i], j);
+    llvalue = strtoll(tmp, NULL, 10);
+    free(tmp);
+    *intvalue = (int)llvalue;
+    end = &end[j];
+    
+    return true;
+}
 
 bool is_special_value(struct sysctlmif_object *object)
 {
     bool special = false;
 
-    // XXX special = strcmp(object->name,"vm.phys_free") == 0;
+    special = strcmp(object->name,"vm.phys_free") == 0;
 
     return special;
 }
@@ -56,37 +89,73 @@ int display_special_value(struct sysctlmif_object *object, void* value, size_t v
 
 static int vm_phys_free(void* value, size_t value_size)
 {
-    int num_domain, num_free_list, error;
-    char *valuestr, *sentinel;
-    char *domain = "DOMAIN";
-    char *free_list = "FREE LIST";
-    char *dashdash = "--";
+    int num_domain, num_list, num_pool, tmp, error;
+    char *start, *end;
+    char *line = NULL;
+    size_t linecap = 0;
+    ssize_t linelen;
+    FILE *fp = fmemopen(value, value_size, "r");
+    bool parselist;
 
     num_domain = error = 0;
-    valuestr = (char*) value;
-
-    //printf("%s\n", value);
+    parselist = false;
+    xo_emit("{L:\n}");
     xo_open_container("value");
-    while( (sentinel = strstr(valuestr, domain)) != NULL) {
-	xo_open_container("Domain");
-	xo_emit("{:num_domain/%d}",num_domain);
-	num_free_list = 0;
-	while( (sentinel = strstr(valuestr, free_list)) != NULL) {
-	    xo_open_container("Free_List");
-	    xo_emit("{:num_free_list/%d}", num_free_list);
-	    xo_close_container("Free_List");
-	    num_free_list++;
-	    valuestr = sentinel;
-	    if(valuestr != NULL)
-		valuestr +=1;
+    /*
+     * XXX "domain" or "freelist" =>c xo_close_containes = <ZZZZZZZZZZZ>
+     */
+    while ((linelen = getline(&line, &linecap, fp)) > 0)
+    {
+	/* DOMAIN X :*/
+	if( strstr(line, "DOMAIN") != NULL) {
+	    if(num_domain > 0)
+		xo_close_container("memorydomain");
+	    xo_open_container("memorydomain");
+	    xo_emit("{L:DOMAIN }{:num_domain/%d}{Lc:}{L:\n\n}", num_domain);
+	    num_domain++;
+	    num_list = 0;
 	}
-	xo_close_container("Domain");
-	num_domain++;
-	valuestr = sentinel;
-	if(valuestr != NULL)
-	    valuestr +=1;
+
+	/* FREE LIST Y :*/
+	if( strstr(line, "FREE LIST") != NULL) {
+	    if(num_list > 0)
+		xo_close_container("list");
+	    xo_open_container("list");
+	    xo_emit("{L:FREE LIST }{:num_list/%d}{Lc:}{L:\n}", num_list);
+	    num_list++;	    
+	    free(line);
+	    getline(&line, &linecap, fp);
+	    xo_emit("{L:\n}");
+	    free(line);
+	    
+	    /*  ORDER (SIZE)  |  NUMBER */
+	    getline(&line, &linecap, fp);
+	    xo_emit_field("L", line, NULL, NULL);
+	    free(line);
+	    /*                |  POOL 0  |  POOL 1 .... */
+	    getline(&line, &linecap, fp);
+	    xo_emit_field("L", line, NULL, NULL);
+	    free(line);
+	    /*--            -- --      -- --      -- ...*/
+	    getline(&line, &linecap, fp);
+	    xo_emit_field("L", line, NULL, NULL);
+	    free(line);
+
+	    /* Rows */
+	    /*getline(&line, &linecap, fp);
+	    xo_open_container("memorder");
+	    //if(find_int(line, end, &tmp))
+	    //	xo_emit("{:num_order/%d}{L:\n}", tmp);
+
+	    xo_close_container("memorder");*/
+
+	}
+
+	free(line);
     }
+    xo_close_container("list");
+    xo_close_container("memorydomain");
     xo_close_container("value");
-    
+
     return error;
 }
