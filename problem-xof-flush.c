7ec331c8ec505a2f350a56f24e3619a968c73e59
Alfonso Siciliano <alfix86@gmail.com>
2019-03-04 01:29:12 +0100

    problem: first value

--- problem-xof-flush.c
+++ problem-xof-flush.c
@@ -1,25 +1,20 @@
 /*
+ *Problem: with xo_set_flags(NULL, XOF_FLUSH) and libxo=text
+ *         the first {:value} is not printed
+ *
  * % cc -g problem-xo-flush.c -lxo -o problem-xo-flush
  *
  *  output xo_set_flags(NULL, XOF_FLUSH):
  *  % ./problem-xo-flush
- *     Value1: Value2:2 Format:fmtstr
- *     Value3:3
- * -"1" of :value1 isn't printed-
+ * TEST 1
+ * Value1: <'1' should be here>
+ * Value2:2 Str1:str1
+ * Value3:3
+ * TEST 2
+ * Value4:4 Str2:str2
+ * TEST 3
+ * Str3:str3 Value5:5
  *
- *  output xo_set_flags(NULL, 0):
- *  % ./problem-xo-flush
- *     Value1:1 Value2:2 Format:fmtstr
- *     Value3:3
- * -OK-
- *
- * output xo_set_flags(NULL, XOF_FLUSH):
- * % ./problem-xo-flush --libxo=xml,pretty
- *   <value1>1</value1>
- *   <value2>2</value2>
- *   <format>fmtstr</format>
- *   <value3>3</value3>
- * -OK- the problem is with libxo=text
  */
 
 #include <libxo/xo.h>
@@ -28,7 +23,13 @@
 int main(int argc, char** argv)
 {
     int value1 = 1, value2 = 2, value3 = 3;
-    char *fmt = "fmtstr";
+    char *str1 = "str1";
+
+    int value4 = 4;
+    char *str2 = "str2";
+
+    int value5 = 5;
+    char *str3 = "str3";
     
     atexit(xo_finish_atexit);
 
@@ -39,12 +40,25 @@ int main(int argc, char** argv)
     if (argc < 0)
 	exit(EXIT_FAILURE);
 
+    xo_emit("{L:TEST 1\n}");
+    /* This is the problem: value1 is not printd */
     xo_emit("{Lc:Value1}{:value1/%d}{P: }", value1);
+    /* with or without '\n' */
+    xo_emit("{L:\n}");
     xo_emit("{Lc:Value2}{:value2/%d}{P: }", value2);
-    xo_emit("{Lc:Format}{:format/%s}", fmt);
+    xo_emit("{Lc:Str1}{:str1/%s}", str1);
     xo_emit("{L:\n}");
     xo_emit("{Lc:Value3}{:value3/%d}", value3);
     xo_emit("{L:\n}");
+
+
+    xo_emit("{L:TEST 2\n}");
+    xo_emit("{Lc:Value4}{:value4/%d}{P: }{Lc:Str2}{:str2/%s}{L:\n}", value4, str2);
+
+    
+    xo_emit("{L:TEST 3\n}");
+    xo_emit("{Lc:Str3}{:str3/%s}{P: }{Lc:Value5}{:value5/%d}{L:\n}", str3, value5);
+
     
     return 0;
 }
