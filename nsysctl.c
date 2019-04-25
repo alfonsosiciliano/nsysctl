7c9448313d3ded08f4920e172485146f14557760
Alfonso Siciliano <alfix86@gmail.com>
2019-04-25 17:39:02 +0200

    fix GCC

--- nsysctl.c
+++ nsysctl.c
@@ -84,21 +84,22 @@ static const struct ctl_flag ctl_flags[NUM_CTLFLAGS] = {
 
 static const char *ctl_typename[CTLTYPE+1] =
 {
-    [CTLTYPE_INT] = "integer",
-    [CTLTYPE_UINT] = "unsigned integer",
-    [CTLTYPE_LONG] = "long integer",
-    [CTLTYPE_ULONG] = "unsigned long",
-    [CTLTYPE_U8] = "uint8_t",
-    [CTLTYPE_U16] = "uint16_t",
-    [CTLTYPE_U32] = "uint32_t",
-    [CTLTYPE_U64] = "uint64_t",
-    [CTLTYPE_S8] = "int8_t",
-    [CTLTYPE_S16] = "int16_t",
-    [CTLTYPE_S32] = "int32_t",
-    [CTLTYPE_S64] = "int64_t",
-    [CTLTYPE_NODE] = "node",
-    [CTLTYPE_STRING] = "string",
-    [CTLTYPE_OPAQUE] = "opaque",
+    "ZEROUNUSED", 
+    "node", 
+    "integer", 
+    "string", 
+    "int64_t", 
+    "opaque", 
+    "unsigned integer", 
+    "long integer", 
+    "unsigned long", 
+    "uint64_t", 
+    "uint8_t", 
+    "uint16_t", 
+    "int8_t",
+    "int16_t", 
+    "int32_t", 
+    "uint32_t" 
 };
 
 void usage()
