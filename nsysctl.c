6efc654dec10031e9eb296ffc78f57fcd31adff5
Alfonso Siciliano <alfix86@gmail.com>
2019-06-02 22:01:23 +0200

    indent

--- nsysctl.c
+++ nsysctl.c
@@ -500,17 +500,17 @@ int display_basic_type(struct sysctlmif_object *object, void *value, size_t valu
 	    else
 		xo_emit("{:value/%d}", ((int*)value)[i]);
 	    break;
-	case CTLTYPE_LONG: 	xo_emit("{:value/%ld}", ((long*)value)[i]);    break;
-	case CTLTYPE_S8:	xo_emit("{:value/%d}", ((int8_t*)value)[i]);	break;
-	case CTLTYPE_S16:	xo_emit("{:value/%d}", ((int16_t*)value)[i]);	break;
-	case CTLTYPE_S32:	xo_emit("{:value/%d}", ((int32_t*)value)[i]);	break;
-	case CTLTYPE_S64:	xo_emit("{:value/%ld}", ((int64_t*)value)[i]);	break;
-	case CTLTYPE_UINT:	xo_emit("{:value/%u}", ((u_int*)value)[i]);	break;
-	case CTLTYPE_ULONG:	xo_emit("{:value/%lu}", ((u_long*)value)[i]);	break;
-	case CTLTYPE_U8:	xo_emit("{:value/%u}", ((uint8_t*)value)[i]);	break;
-	case CTLTYPE_U16:	xo_emit("{:value/%u}", ((uint16_t*)value)[i]);	break;
-	case CTLTYPE_U32:	xo_emit("{:value/%u}", ((uint32_t*)value)[i]);	break;
-	case CTLTYPE_U64:	xo_emit("{:value/%lu}", ((uint64_t*)value)[i]);	break;
+	case CTLTYPE_LONG:  xo_emit("{:value/%ld}", ((long*)value)[i]);    break;
+	case CTLTYPE_S8:    xo_emit("{:value/%d}",  ((int8_t*)value)[i]);  break;
+	case CTLTYPE_S16:   xo_emit("{:value/%d}",  ((int16_t*)value)[i]); break;
+	case CTLTYPE_S32:   xo_emit("{:value/%d}",  ((int32_t*)value)[i]); break;
+	case CTLTYPE_S64:   xo_emit("{:value/%ld}", ((int64_t*)value)[i]); break;
+	case CTLTYPE_UINT:  xo_emit("{:value/%u}",  ((u_int*)value)[i]);   break;
+	case CTLTYPE_ULONG: xo_emit("{:value/%lu}", ((u_long*)value)[i]);  break;
+	case CTLTYPE_U8:    xo_emit("{:value/%u}",  ((uint8_t*)value)[i]); break;
+	case CTLTYPE_U16:   xo_emit("{:value/%u}",  ((uint16_t*)value)[i]);break;
+	case CTLTYPE_U32:   xo_emit("{:value/%u}",  ((uint32_t*)value)[i]);break;
+	case CTLTYPE_U64:   xo_emit("{:value/%lu}", ((uint64_t*)value)[i]);break;
 	default:
 	    xo_warnx("'%s' unknown type", object->name);
 	    error++;
@@ -601,17 +601,17 @@ int set_basic_value(struct sysctlmif_object *object, char *input)
 		    ((int *)newval)[i] = (int)strtoll(start, NULL, 10); 
 		}
 		break;
-	    case CTLTYPE_LONG:  ((long *)newval)[i] = (long)strtoll(start, NULL, 10);     break;
-	    case CTLTYPE_S8:    ((int8_t *)newval)[i] = (int8_t)strtoll(start, NULL, 10);   break;
-	    case CTLTYPE_S16:   ((int16_t *)newval)[i] = (int16_t)strtoll(start, NULL, 10);  break;
-	    case CTLTYPE_S32:   ((int32_t *)newval)[i] = (int32_t)strtoll(start, NULL, 10);  break;
-	    case CTLTYPE_S64:   ((int64_t *)newval)[i] = (int64_t)strtoll(start, NULL, 10);  break;
-	    case CTLTYPE_UINT:  ((u_int *)newval)[i] = (u_int)strtoull(start, NULL, 10);    break;
-	    case CTLTYPE_ULONG: ((u_long *)newval)[i] = (u_long)strtoull(start, NULL, 10);   break;
-	    case CTLTYPE_U8:    ((uint8_t *)newval)[i] = (uint8_t)strtoull(start, NULL, 10);  break;
-	    case CTLTYPE_U16:   ((uint16_t *)newval)[i] = (uint16_t)strtoull(start, NULL, 10); break;
-	    case CTLTYPE_U32:   ((uint32_t *)newval)[i] = (uint32_t)strtoull(start, NULL, 10); break;
-	    case CTLTYPE_U64:   ((uint64_t *)newval)[i] = (uint64_t)strtoull(start, NULL, 10); break;
+	    case CTLTYPE_LONG:  ((long *)newval)[i]     = (long)strtoll(start, NULL, 10);     break;
+	    case CTLTYPE_S8:    ((int8_t *)newval)[i]   = (int8_t)strtoll(start, NULL, 10);   break;
+	    case CTLTYPE_S16:   ((int16_t *)newval)[i]  = (int16_t)strtoll(start, NULL, 10);  break;
+	    case CTLTYPE_S32:   ((int32_t *)newval)[i]  = (int32_t)strtoll(start, NULL, 10);  break;
+	    case CTLTYPE_S64:   ((int64_t *)newval)[i]  = (int64_t)strtoll(start, NULL, 10);  break;
+	    case CTLTYPE_UINT:  ((u_int *)newval)[i]    = (u_int)strtoull(start, NULL, 10);   break;
+	    case CTLTYPE_ULONG: ((u_long *)newval)[i]   = (u_long)strtoull(start, NULL, 10);  break;
+	    case CTLTYPE_U8:    ((uint8_t *)newval)[i]  = (uint8_t)strtoull(start, NULL, 10); break;
+	    case CTLTYPE_U16:   ((uint16_t *)newval)[i] = (uint16_t)strtoull(start, NULL, 10);break;
+	    case CTLTYPE_U32:   ((uint32_t *)newval)[i] = (uint32_t)strtoull(start, NULL, 10);break;
+	    case CTLTYPE_U64:   ((uint64_t *)newval)[i] = (uint64_t)strtoull(start, NULL, 10);break;
 	    default:
 		xo_emit("{L:\n}");
 		xo_warnx("Unknown type '%s'", object->name);
