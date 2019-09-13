6c02805a1dd2899a6abee6b3f40fc69806d8ea33
Alfonso Siciliano <alfix86@gmail.com>
2019-09-13 23:28:59 +0200

    update copyright

--- src/sysctlinfo_helper.h
+++ src/sysctlinfo_helper.h
@@ -1,5 +1,7 @@
-/*
- * Copyright (c) 2019 Alfonso Sabato Siciliano https://alfix.gitlab.io
+/*-
+ * SPDX-License-Identifier: BSD-2-Clause
+ *
+ * Copyright (c) 2019 Alfonso Sabato Siciliano
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
