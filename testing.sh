3494c824c74b479d54e6f52a23489b2c12ceabe4
Alfonso Siciliano <alfix86@gmail.com>
2019-09-14 00:01:04 +0200

    update

--- testing.sh
+++ testing.sh
@@ -43,8 +43,8 @@ done
 echo "sysctl -a"
 sysctl -a >> sysctl.txt
 ./nsysctl -aNV >> nsysctl.txt
-#diff sysctl.txt nsysctl.txt | diffstat -m
+diff sysctl.txt nsysctl.txt | diffstat -m
 #diff -y -W 120 sysctl.txt nsysctl.txt 
-meld sysctl.txt nsysctl.txt
+#meld sysctl.txt nsysctl.txt
 rm sysctl.txt nsysctl.txt
 
